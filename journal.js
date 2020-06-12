7a176f9e062d1e54f911b4a02e23d55978e6bd2e
Rosny Ratsianompo <rosny.rat@onja.org>
2020-06-12 12:03:43 +0100

    edited journal.js

--- journal.js
+++ journal.js
@@ -1,1 +1,68 @@
-console.log('it works');
+// console.log('it works');
+class Journal {
+    constructor(title, content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // title = "My Onja journal";
+        // content = {
+        //     date : "Monday the twelve of june 2020",
+        //     name : "Onja",
+        //     type : "NGO",
+        //     fund : "Mr. Samuel"
+        // };
+    // }
+    // start() {
+    //     let startMenu1= Math.floor((Math.random() * title));
+    //     let list1 = title[startMenu1];
+    //     let startMenu2 = Math.floor((Math.random() * content.length));
+    //     let list2 = content[startMenu2];
+    //     let lists = `${list1} ${list2}`;
+    //     return lists;
+    //      }
+    //     }
+    // let choice = new Journal ();
+    // choice.start(); 
+    // /* Adding method brake() later to the created object */
+    // choice.brake = function() {
+    //     console.log("Applying Brake...");
+    // }
+    // choice.brake()
+    // //Let the user to choose their prefering quote between 1 or 2:
+    //     const startMenu = prompt("choose (1) for listing all the entries");
+    //        if (startMenu === "1") {
+    //            console.log(star());
+    //          } else if (startMenu === "2") {
+    //            console.log(start());
+    //         }
+        
+
+//     const startMenu = 
+//     if (startMenu === 1) {
+//     for (let i = 0; i < startMenu; i++) {
+//         const journalMenu = () => {
+//             let list1 = content;
+//             console.log(list1);
+//         journalMenu();
+// }
+// }
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
