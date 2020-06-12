7a45caefd69ee34accc9b38818c2e2b142a9dea7
Rosny Ratsianompo <rosny.rat@onja.org>
2020-06-12 13:40:58 +0100

    journal.js

--- journal.js
+++ journal.js
@@ -1,68 +1,10 @@
-// console.log('it works');
-class Journal {
-    constructor(title, content) {
-        this.title = title;
-        this.content = content;
+let journal = Number(prompt("Welcome to my journal\n choose(1) for listing all  the entries\n Choose (2) for adding a new entry\n Choose (3) to quit"));
+
+for (let i = 0; i < journal; i++) {
+    if (journal === 1) {
+        console.log(Number(prompt("Title: My first blog \n content: This is one the amazing blog which I have ever seen in my life, have a read whenever you got time.")));
+    } else if (journal === 2){
+        console.log(Number(prompt("Title: God is my favour \n Content: To rise me from my weakness , to lead me into the rigth way, and to cover me from my illness is Jesus.")))
     }
-
-    // title = "My Onja journal";
-        // content = {
-        //     date : "Monday the twelve of june 2020",
-        //     name : "Onja",
-        //     type : "NGO",
-        //     fund : "Mr. Samuel"
-        // };
-    // }
-    // start() {
-    //     let startMenu1= Math.floor((Math.random() * title));
-    //     let list1 = title[startMenu1];
-    //     let startMenu2 = Math.floor((Math.random() * content.length));
-    //     let list2 = content[startMenu2];
-    //     let lists = `${list1} ${list2}`;
-    //     return lists;
-    //      }
-    //     }
-    // let choice = new Journal ();
-    // choice.start(); 
-    // /* Adding method brake() later to the created object */
-    // choice.brake = function() {
-    //     console.log("Applying Brake...");
-    // }
-    // choice.brake()
-    // //Let the user to choose their prefering quote between 1 or 2:
-    //     const startMenu = prompt("choose (1) for listing all the entries");
-    //        if (startMenu === "1") {
-    //            console.log(star());
-    //          } else if (startMenu === "2") {
-    //            console.log(start());
-    //         }
-        
-
-//     const startMenu = 
-//     if (startMenu === 1) {
-//     for (let i = 0; i < startMenu; i++) {
-//         const journalMenu = () => {
-//             let list1 = content;
-//             console.log(list1);
-//         journalMenu();
-// }
-// }
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+}
