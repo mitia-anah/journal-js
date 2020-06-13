71e4390606f43efb0b54342bd06ee1d7c5d6c138
Rosny Ratsianompo <rosny.rat@onja.org>
2020-06-13 17:03:52 +0100

    edited the the file.js

--- journal.js
+++ journal.js
@@ -1,10 +1,27 @@
-let journal = Number(prompt("Welcome to my journal\n choose(1) for listing all  the entries\n Choose (2) for adding a new entry\n Choose (3) to quit"));
+let journalArray = [
+    {
+        title : "My first blog",
+        content : "This is one the amazing blog which I have ever seen in my life, have a read whenever you got time."
+    },
 
+    {
+       titlte : "God is my favour",
+       content : "To rise me from my weakness , to lead me into the rigth way, and to cover me from my illness is Jesus."
+    },
+
+    {
+        title : "Sky is the limite",
+        content: "Don't let anyone to have your crow, keep it 'till jesus's coming. "
+    }
+];
+ss
+let journal = Number(prompt("Welcome to my journal\n choose(1) for listing all  the entries\n Choose (2) for adding a new entry\n Choose (3) to quit"));
+let journal = journalArray;
 for (let i = 0; i < journal; i++) {
     if (journal === 1) {
-        console.log(Number(prompt("Title: My first blog \n content: This is one the amazing blog which I have ever seen in my life, have a read whenever you got time.")));
+        alert(`${}`);
     } else if (journal === 2){
-        console.log(Number(prompt("Title: God is my favour \n Content: To rise me from my weakness , to lead me into the rigth way, and to cover me from my illness is Jesus.")))
+        
     }
     
 }
