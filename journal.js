0811bb88cd2947dd8ee4f57591b49817c67f6ccb
Rosny Ratsianompo <rosny.rat@onja.org>
2020-06-15 12:20:19 +0100

    edited journal.js

--- journal.js
+++ journal.js
@@ -1,3 +1,4 @@
+// arrays of object have been created here
 const journalArray = [
     {
         title : "My first entry",
@@ -7,14 +8,17 @@ const journalArray = [
     {
        titlte : "My second entry",
        content : "To rise me from my weakness , to lead me into the rigth way, and to cover me from my illness is Jesus."
-    },
+    }
+];
 
+const newEntry = [
     {
         title : "Sky is the limite",
         content: "Don't let anyone to have your crow, keep it 'till jesus's coming. "
     }
 ];
 
+// This is prompt to show the user's choice 
  let journal = Number(prompt("Welcome to my journal\n choose(1) for listing all  the entries\n Choose (2) for adding a new entry\n Choose (3) to quit"));
    for (let i = 0; i < journalArray.length; i++) {
     let myEntries = journalArray[i];
@@ -22,9 +26,13 @@ const journalArray = [
     if (journal === 1) {
      alert(property + " " + ':' + " " + myEntries[property] );
      } else if (journal === 2) {
-        alert(`Title : My new entry \n content :  smile is everything you’ll never understand.`);
-     } else {
+         let newJournalEntry = newEntry[i]; 
+         for (let property in newJournalEntry )
+        alert(property + " " + ':' + " " + newJournalEntry[property] );
+     } else if (journal === 3) {
          alert(`just leave the site!`)
+     } else if (journal === 4) {
+
      }
     }
 }
