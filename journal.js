bd053be60f7f1807082a8c24826fcc63db3790e0
Rosny Ratsianompo <rosny.rat@onja.org>
2020-06-15 09:51:31 +0100

    edited the journal.js

--- journal.js
+++ journal.js
@@ -1,11 +1,11 @@
-let journalArray = [
+const journalArray = [
     {
         title : "My first blog",
         content : "This is one the amazing blog which I have ever seen in my life, have a read whenever you got time."
     },
 
     {
-       titlte : "God is my favour",
+       titlte : "My new entry",
        content : "To rise me from my weakness , to lead me into the rigth way, and to cover me from my illness is Jesus."
     },
 
@@ -14,14 +14,18 @@ let journalArray = [
         content: "Don't let anyone to have your crow, keep it 'till jesus's coming. "
     }
 ];
-ss
-let journal = Number(prompt("Welcome to my journal\n choose(1) for listing all  the entries\n Choose (2) for adding a new entry\n Choose (3) to quit"));
-let journal = journalArray;
-for (let i = 0; i < journal; i++) {
+
+ let journal = Number(prompt("Welcome to my journal\n choose(1) for listing all  the entries\n Choose (2) for adding a new entry\n Choose (3) to quit"));
+   for (let i = 0; i < journalArray.length; i++) {
+    let myEntries = journalArray[i];
+    for (let property in myEntries) {
+    //   alert(property + '=' + myEntries[property]);
     if (journal === 1) {
-        alert(`${}`);
-    } else if (journal === 2){
-        
+     alert(property + '=' + myEntries[property]);
+     } else if(journal === 2) {
+         alert(`2`);
+     } else if (journal === 3) {
+         alert(3);
+     }
     }
-    
 }
