b08e3293a83e35f5edc57a43390a0bc4b5d1f24e
Rosny Ratsianompo <rosny.rat@onja.org>
2020-06-16 15:48:24 +0100

    edited journal.js

--- journal.js
+++ journal.js
@@ -11,28 +11,53 @@ const journalArray = [
     }
 ];
 
-const newEntry = [
-    {
-        title : "Sky is the limite",
-        content: "Don't let anyone to have your crow, keep it 'till jesus's coming. "
-    }
-];
-
 // This is prompt to show the user's choice 
- let journal = Number(prompt("Welcome to my journal\n choose(1) for listing all  the entries\n Choose (2) for adding a new entry\n Choose (3) to quit"));
-   for (let i = 0; i < journalArray.length; i++) {
-    let myEntries = journalArray[i];
-    for (let property in myEntries) {
-    if (journal === 1) {
-     alert(property + " " + ':' + " " + myEntries[property] );
-     } else if (journal === 2) {
-         let newJournalEntry = newEntry[i]; 
-         for (let property in newJournalEntry )
-        alert(property + " " + ':' + " " + newJournalEntry[property] );
-     } else if (journal === 3) {
-         alert(`just leave the site!`)
-     } else if (journal === 4) {
+  let journal = prompt(`
+  Welcome to my journal
 
-     }
-    }
-}
+  choose(1) for listing all  the entries
+  Choose (2) for adding a new entry
+  Choose (3) to quit
+  choose
+
+  `);
+  alert(journal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Number(prompt("Welcome to my journal\n choose(1) for listing all  the entries\n Choose (2) for adding a new entry\n Choose (3) to quit"));
+//    for (let i = 0; i < journalArray.length; i++) {
+//     let myEntries = journalArray[i];
+//     for (let property in myEntries) {
+//     if (journal === 1) {
+//      alert(property + " " + ':' + " " + myEntries[property] );
+//      } else if (journal === 2) {
+//         let entryTitle = prompt("Enter your title:"); 
+//         let entryContent = prompt("Enter your content:");
+//         let entry = {
+//             title: entryTitle,
+//             content: entryContent,
+//         };
+//         journalArray.push(entry);
+//      } else if (journal === 3) {
+//          alert(`just leave the site!`);
+//      }
+//     }
+// }
