dc54bc5110c82ece15f69683c053fcd38a41d1d0
Rosny Ratsianompo <rosny.rat@onja.org>
2020-06-20 10:34:29 +0100

    updated the journal.js

--- journal.js
+++ journal.js
@@ -1,63 +1,68 @@
 // arrays of object have been created here
-const journalArray = [
-    {
-        title : "My first entry",
-        content : "This is one the amazing blog which I have ever seen in my life, have a read whenever you got time."
-    },
+    let journal = [
+        {
+            title : "My first journal",
+            content : "Shool news"      },
+        {
+            title : "My second journal",
+            content : "Wonderful day"
+        },
+    ];
+    console.log(journal);
+   // This prompt let the user to choose until they quit
+    let entryMenu = prompt(`
+    Welcom to my onja journal!
 
-    {
-       titlte : "My second entry",
-       content : "To rise me from my weakness , to lead me into the rigth way, and to cover me from my illness is Jesus."
+    Choose (1) for listing all the entries.
+    Choose (2) fro adding a new entry.
+    Choose (3) to quit.
+    Choose (4) to delete the last entry.
+    Choose (5) to delete a specific entry. (with its index)
+    `);
+
+    // to show the list of the journal
+    switch (entryMenu) {
+        case "1":
+            for (let i = 0; i < journal.length; i++) {
+                let myJournal = `
+                Title : ${journal[i].title}
+                content : ${journal[i].content}
+                `;
+                alert(myJournal);
+            }
+            break;
+        case "2": 
+            // the user asked to enter new entry to add new array
+            let entryTitle = prompt("Enter the journal title:");
+            let entryContent = prompt("Enter the journal content")
+
+            let entry = {
+                title : entryTitle,
+                content : entryContent,
+            }
+            journal.push(entry);
+            for (let i = 0; i < journal.length; i++) {
+                let myJournal = `
+                Title : ${journal[i].title}
+                content : ${journal[i].content}
+                `;
+                    alert(myJournal);
+        }
+            break;
+        case "3": 
+            alert(`Want to quit? (press Ok)`);
+            break;
+        case "4":
+            entryDeleted = journal.pop();
+            alert(`${entryDeleted.title} last entry deleted.`);
+            break;
+        case "5": 
+        // Remove an existing link from array
+        let journalToRemove = Number(
+            prompt(`Enter a specific number you want to delete: (between 0 to 2)`)
+        );
+        let removedjournal = journal.splice(journalToRemove--, 1);
+            alert(`link ${removedjournal[0].title} removed succefully`);
+            break;
+        default:
     }
-];
-
-// This is prompt to show the user's choice 
-  let journal = prompt(`
-  Welcome to my journal
-
-  choose(1) for listing all  the entries
-  Choose (2) for adding a new entry
-  Choose (3) to quit
-  choose
-
-  `);
-  alert(journal);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Number(prompt("Welcome to my journal\n choose(1) for listing all  the entries\n Choose (2) for adding a new entry\n Choose (3) to quit"));
-//    for (let i = 0; i < journalArray.length; i++) {
-//     let myEntries = journalArray[i];
-//     for (let property in myEntries) {
-//     if (journal === 1) {
-//      alert(property + " " + ':' + " " + myEntries[property] );
-//      } else if (journal === 2) {
-//         let entryTitle = prompt("Enter your title:"); 
-//         let entryContent = prompt("Enter your content:");
-//         let entry = {
-//             title: entryTitle,
-//             content: entryContent,
-//         };
-//         journalArray.push(entry);
-//      } else if (journal === 3) {
-//          alert(`just leave the site!`);
-//      }
-//     }
-// }
